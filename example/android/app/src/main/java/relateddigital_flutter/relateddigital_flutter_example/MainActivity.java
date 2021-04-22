f658ea29e36df0ef0d88a3fa6e1b3fd737a87e2f
cicimen <egemengulkilik@gmail.com>
2021-04-22 18:35:14 +0300

    initial commit

---
+++ example/android/app/src/main/java/relateddigital_flutter/relateddigital_flutter_example/MainActivity.java
@@ -0,0 +1,6 @@
+package relateddigital_flutter.relateddigital_flutter_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/RelateddigitalFlutterPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface RelateddigitalFlutterPlugin : NSObject<FlutterPlugin>
+@end
