198d2873c3ad893e614ea7a05288802d8e3d3b9f
cicimen <egemengulkilik@gmail.com>
2021-04-27 17:33:50 +0300

    relateddigitalplugin.java add for android

--- android/src/main/java/com/relateddigital/flutter/RelatedDigitalPlugin.java
+++ android/src/main/java/com/relateddigital/flutter/RelatedDigitalPlugin.java
@@ -1,4 +1,4 @@
-package relateddigital_flutter.relateddigital_flutter;
+package com.relateddigital.flutter;
 
 import androidx.annotation.NonNull;
 
@@ -9,8 +9,8 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** RelateddigitalFlutterPlugin */
-public class RelateddigitalFlutterPlugin implements FlutterPlugin, MethodCallHandler {
+/** RelatedDigitalPlugin */
+public class RelatedDigitalPlugin implements FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
