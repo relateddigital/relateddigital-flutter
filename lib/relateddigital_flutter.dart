3a080006e4fddc294a0ae853c2d653a6e8ca29a3
cicimen <egemengulkilik@gmail.com>
2021-04-27 11:10:56 +0300

    untrivial change

--- lib/relateddigital_flutter.dart
+++ lib/relateddigital_flutter.dart
@@ -8,6 +8,7 @@ class RelateddigitalFlutter {
       const MethodChannel('relateddigital_flutter');
 
   static Future<String> get platformVersion async {
+    // change
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
