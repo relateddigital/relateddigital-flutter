5584292e54c9e80fb83c7bc09f766ed8303a3830
cicimen <egemengulkilik@gmail.com>
2021-04-27 14:51:49 +0300

    constants.dart file add

---
+++ lib/constants.dart
@@ -0,0 +1,25 @@
+class Constants {
+  static const String CHANNEL_NAME = 'RelatedDigitalPlugin';
+
+  static const String M_INIT = 'RD/init';
+  static const String M_PERMISSION = 'RD/requestPermission';
+  static const String M_TOKEN_RETRIEVED = 'RD/getToken';
+  static const String M_NOTIFICATION_OPENED = 'RD/openNotification';
+  static const String M_EURO_USER_ID = 'RD/setEuroUserId';
+  static const String M_EMAIL_WITH_PERMISSION = 'RD/setEmail';
+  static const String M_USER_PROPERTY = 'RD/setUserProperty';
+  static const String M_APP_VERSION = 'RD/setAppVersion';
+  static const String M_NOTIFICATION_PERMISSION = 'RD/setPushNotificationPermission';
+  static const String M_EMAIL_PERMISSION = 'RD/setEmailPermission';
+  static const String M_PHONE_PERMISSION = 'RD/setPhoneNumberPermission';
+  static const String M_BADGE = 'RD/setBadgeCount';
+  static const String M_ADVERTISING = 'RD/setAdvertisingIdentifier';
+  static const String M_TWITTER = 'RD/setTwitterId';
+  static const String M_FACEBOOK = 'RD/setFacebookId';
+  static const String M_CUSTOM_EVENT = 'VL/customEvent';
+  static const String M_REGISTER_EMAIL = 'RD/registerEmail';
+
+  static const String VL_TOKEN_KEY = 'RegisterToken';
+  static const String VL_TOKEN_PARAM = 'OM.sys.TokenID';
+  static const String VL_APP_ID_PARAM = 'OM.sys.AppID';
+}
