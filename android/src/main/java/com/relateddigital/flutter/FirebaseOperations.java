5a9d4f96ff7f94796c14d597d661c22254171b05
Barış Arslan <bariisarslans@hotmail.com>
2021-05-21 15:04:30 +0300

    Updates native FirebaseOperations class

--- android/src/main/java/com/relateddigital/flutter/FirebaseOperations.java
+++ android/src/main/java/com/relateddigital/flutter/FirebaseOperations.java
@@ -1,46 +1,44 @@
 package com.relateddigital.flutter;
-
+​
 import android.content.Context;
-
+​
 import androidx.annotation.NonNull;
-
+​
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
-
+import com.google.firebase.messaging.FirebaseMessaging;
 import java.util.HashMap;
 import java.util.Map;
-
+​
 import euromsg.com.euromobileandroid.EuroMobileManager;
 import io.flutter.plugin.common.MethodChannel;
-
+​
 public class FirebaseOperations {
     private final Context mContext;
     private final MethodChannel mChannel;
-
+​
     public FirebaseOperations(Context context, MethodChannel channel) {
         mContext = context;
         mChannel = channel;
     }
-
+​
     public void setExistingFirebaseTokenToEuroMessage() {
-        FirebaseInstanceId.getInstance().getInstanceId()
-                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+​
+​
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
                     @Override
-                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
-
+                    public void onComplete(@NonNull Task<String> task) {
                         if (!task.isSuccessful()) {
                             return;
                         }
-
-                        String token = task.getResult().getToken();
+                        String token = task.getResult();
                         EuroMobileManager.getInstance().subscribe(token, mContext);
-
+​
                         Map <String, Object> result = new HashMap<>();
                         result.put("deviceToken", token);
                         result.put("playServiceEnabled", EuroMobileManager.checkPlayService(mContext));
-
+​
                         mChannel.invokeMethod(Constants.M_TOKEN_RETRIEVED, result);
                     }
                 });
