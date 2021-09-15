36156c527076426e6149fbcdbe5323a9bb65ca7e
cicimen <egemengulkilik@gmail.com>
2021-09-15 03:33:15 +0300

    example project NotificationServiceExtension and NotificationContentExtension update

--- example/ios/NotificationContent/EMNotificationViewController.swift
+++ example/ios/NotificationContent/EMNotificationViewController.swift
@@ -3,47 +3,48 @@ import UserNotifications
 import UserNotificationsUI
 import Euromsg
 
-@available(iOS 10.0, *)
 @objc(EMNotificationViewController)
 class EMNotificationViewController: UIViewController, UNNotificationContentExtension {
-
-		let appUrl = URL(string: "euromsgExample://")
-		let carouselView = EMNotificationCarousel.initView()
-		var completion: ((_ url: URL?, _ userInfo: [AnyHashable: Any]?) -> Void)?
-		func didReceive(_ notification: UNNotification) {
-				carouselView.didReceive(notification)
-		}
-		func didReceive(_ response: UNNotificationResponse,
-										completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
-				carouselView.didReceive(response, completionHandler: completion)
-		}
-		override func loadView() {
-				completion = { [weak self] url, userInfo in
-						if let url = url {
-								self?.extensionContext?.open(url)
-								if url.scheme != self?.appUrl?.scheme, let userInfo = userInfo {
-										Euromsg.handlePush(pushDictionary: userInfo)
-								}
-						}
-						else if let url = self?.appUrl {
-								self?.extensionContext?.open(url)
-						}
-				}
-				carouselView.completion = completion
-				//Add if you want to track which element has been selected
-				carouselView.delegate = self
-				self.view = carouselView
-		}
+    
+    let appUrl = URL(string: "euromsgExample://")
+    let carouselView = EMNotificationCarousel.initView()
+    var completion: ((_ url: URL?, _ userInfo: [AnyHashable: Any]?) -> Void)?
+    
+    
+    func didReceive(_ notification: UNNotification) {
+        Euromsg.configure(appAlias: "relateddigital-flutter-example-ios", enableLog: true)
+        carouselView.didReceive(notification)
+    }
+    func didReceive(_ response: UNNotificationResponse,
+                    completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
+        carouselView.didReceive(response, completionHandler: completion)
+    }
+    override func loadView() {
+        completion = { [weak self] url, userInfo in
+            if let url = url {
+                self?.extensionContext?.open(url)
+                if url.scheme != self?.appUrl?.scheme, let userInfo = userInfo {
+                    Euromsg.handlePush(pushDictionary: userInfo)
+                }
+            } else if let url = self?.appUrl {
+                self?.extensionContext?.open(url)
+            }
+        }
+        carouselView.completion = completion
+        // Add if you want to track which element has been selected
+        carouselView.delegate = self
+        self.view = carouselView
+    }
 }
 
 /**
  Add if you want to track which carousel element has been selected
  */
 extension EMNotificationViewController: CarouselDelegate {
-		
-		func selectedItem(_ element: EMMessage.Element) {
-				//Add your work...
-				print("Selected element is => \(element)")
-		}
-		
+
+    func selectedItem(_ element: EMMessage.Element) {
+        // Add your work...
+        print("Selected element is => \(element)")
+    }
+
 }
--- example/ios/NotificationService/NotificationService.swift
+++ example/ios/NotificationService/NotificationService.swift
@@ -3,21 +3,24 @@ import Euromsg
 
 class NotificationService: UNNotificationServiceExtension {
 
-		var contentHandler: ((UNNotificationContent) -> Void)?
-		var bestAttemptContent: UNMutableNotificationContent?
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
 
-		override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-				self.contentHandler = contentHandler
-				bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-				Euromsg.didReceive(bestAttemptContent, withContentHandler: contentHandler)
-		}
-		
-		override func serviceExtensionTimeWillExpire() {
-				// Called just before the extension will be terminated by the system.
-				// Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-				if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-						Euromsg.didReceive(bestAttemptContent, withContentHandler: contentHandler)
-				}
-		}
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        Euromsg.configure(appAlias: "relateddigital-flutter-example-ios", enableLog: true)
+        Euromsg.didReceive(bestAttemptContent, withContentHandler: contentHandler)
+    }
+
+    override func serviceExtensionTimeWillExpire() {
+        guard let contentHandler = self.contentHandler else {
+            return;
+        }
+        guard let bestAttemptContent = self.bestAttemptContent else {
+            return;
+        }
+        contentHandler(bestAttemptContent)
+    }
 
 }
